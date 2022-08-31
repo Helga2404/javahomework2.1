4579827681aeeb220797fdec30fae753334e0264
Olga Wagner <rosamarena24@gmail.com>
2022-08-31 20:42:36 +0700

    add Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+
+        int cost = 10000;
+        int bonusMile = 20;
+
+        int totalMiles = cost / bonusMile;
+
+        System.out.println(totalMiles);
+
+    }
 }
